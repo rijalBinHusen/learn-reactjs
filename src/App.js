860c1f360b7f0017e2d1f5c083796c0e4fc9e193
rijal <jeryzal333@gmail.com>
2021-10-06 08:46:11 +0700

    mapping array and render to dom or may on vue this call v-for

--- src/App.js
+++ src/App.js
@@ -18,26 +18,24 @@ function FotoPorduct() {
 }
 
 function ProductInfo(props) {
-  const {category, name, isDiscount} = props
-  const price = 7893121;
+  const {category, name, isDiscount} = props;
+  const benefit = ["Tidak kusut terkena air", "Tidak seperti ini", "Tidak seperti itu"];
+  const listBenefits = benefit.map((value) => <li>{value}</li>)
 
   return (
     <div class="Deskripsi">
       <p className="Category">{category}</p>
       <p className="Title">{name}</p>
-      <p className="Price">IDR {price}</p>
+      <p className="Price">IDR 7.893.121</p>
       <CheckDiscount isDiscount={isDiscount} />
       <p className="Info">
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem
+        since the 1500s
       </p>
+      <ul>
+        <li>{listBenefits}</li>
+      </ul>
       <a onClick={ (e) => TambahCart(name, e) } href="#"> Tambah cart</a>
     </div>
   );
