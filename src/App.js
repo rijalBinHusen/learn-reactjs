d72c9332079d993440ba7fef4f58520cb9d3e415
rijal <jeryzal333@gmail.com>
2021-10-06 07:57:35 +0700

    design part 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
       <div class="Foto">
         <img src="sneakers.jpeg" alt="sneakers" />
       </div>
-      <div class="Deskripsi"></div>
+      <div class="Deskripsi">
+        <p class="Category">SNEAKERS</p>
+        <p class="Title">All star</p>
+        <p class="Price">IDR 7.309.389</p>
+        <p class="Info">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
+        </p>
+      </div>
     </div>
   );
 }
