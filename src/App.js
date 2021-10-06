8d5d154e344f738fc48f9f0c0811653feade6240
rijal <jeryzal333@gmail.com>
2021-10-06 07:44:35 +0700

    design part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>Paragraph</p>
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <h5>Heading 5</h5>
-      <h6>Heading 6</h6>
+    <div class="ParetBox">
+      <div class="Foto">
+        <img src="sneakers.jpeg" alt="sneakers" />
+      </div>
+      <div class="Deskripsi"></div>
     </div>
   );
 }
