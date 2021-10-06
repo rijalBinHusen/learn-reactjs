6ad9aa522f2bf9bd67d55f26658a0331bf466b4f
rijal <jeryzal333@gmail.com>
2021-10-06 07:59:51 +0700

    dynamic category and price

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 
 function App() {
+
+  const category = "Sneakers"
+  const price = 7893121
   return (
     <div class="ParetBox">
       <div class="Foto">
         <img src="sneakers.jpeg" alt="sneakers" />
       </div>
       <div class="Deskripsi">
-        <p class="Category">SNEAKERS</p>
+        <p class="Category">{category}</p>
         <p class="Title">All star</p>
-        <p class="Price">IDR 7.309.389</p>
+        <p class="Price">IDR {price}</p>
         <p class="Info">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
         </p>
